13b6b8236b9483be3019735507d9b5f4802286ee
Miikka Karkaus <mkarkaus@c1r4p6.hive.fi>
2019-11-19 19:47:22 +0200

    ..2

--- get_next_line.c
+++ get_next_line.c
@@ -6,59 +6,60 @@
 /*   By: mkarkaus <mkarkaus@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 13:03:14 by mkarkaus          #+#    #+#             */
-/*   Updated: 2019/11/15 17:07:59 by mkarkaus         ###   ########.fr       */
+/*   Updated: 2019/11/19 19:42:55 by mkarkaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft/libft.h"
-#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 void		ft_makeline(char **line, char **tab, const int fd)
 {
 	size_t	k;
-	char	*temp = NULL;
+	char	*temp;
 
 	k = 0;
-	while (*tab[fd] == '\n')
-		tab[fd]++;
+	temp = NULL;
 	while (tab[fd][k] != '\n' && tab[fd][k] != '\0')
 		k++;
 	if (tab[fd][k] == '\n')
 	{
 		*line = ft_strsub(tab[fd], 0, k);
 		temp = ft_strdup(tab[fd] + (k + 1));
-		ft_strclr(tab[fd]);
+		free(tab[fd]);
 		tab[fd] = temp;
 	}
 	else
 	{
 		*line = ft_strdup(tab[fd]);
-		ft_strclr(tab[fd]);
+		free(tab[fd]);
 	}
 }
+
 int			get_next_line(const int fd, char **line)
 {
 	ssize_t		ret;
-	static char	*tab[4000];
+	static char	*tab[MAX_FD];
+	char		*temp;
 	char		buf[BUFF_SIZE + 1];
 
 	ret = 0;
-	if (fd < 0 || !line || BUFF_SIZE < 1)
+	if (fd < 0 || fd > MAX_FD || !line || BUFF_SIZE < 1)
 		return (-1);
+	if (tab[fd] == NULL)
+		tab[fd] = ft_strnew(0);
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		if (tab[fd] != NULL)
-			tab[fd] = ft_strjoin(tab[fd], buf);
-		if (tab[fd] == NULL)
-			tab[fd] = ft_strdup(buf);
+		temp = ft_strjoin(tab[fd], buf);
+		free(tab[fd]);
+		tab[fd] = temp;
+		if (ft_strchr(tab[fd], '\n') != NULL)
+			break ;
 	}
-	if (!tab[fd] && ret == 0)
-		return (0);
 	ft_makeline(line, tab, fd);
+	if (tab[fd][0] == '\0' && ret == 0)
+		return (0);
 	return (1);
 }
--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: mkarkaus <mkarkaus@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 16:54:39 by mkarkaus          #+#    #+#             */
-/*   Updated: 2019/11/15 17:16:38 by mkarkaus         ###   ########.fr       */
+/*   Updated: 2019/11/19 19:40:38 by mkarkaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft/libft.h"
 #include <fcntl.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int		main()
@@ -22,10 +23,14 @@ int		main()
 	int	i;
 	char	*line;
 
+//	line = NULL;
 	i = 0;
-	fd = open("some_lines.txt", O_RDONLY);
-	fd2 = open("text1", O_RDONLY);
-	line = "nappi";
+	fd = open("text1", O_RDONLY);
+	fd2 = open("text4", O_RDONLY);
+	line = "nuppi";
+//	get_next_line(fd, &line);
+//	printf("%s", line);
+//	free(line);
 	while (line)
 	{
 		if (i % 2 == 0)
