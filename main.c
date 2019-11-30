f06cbd751534c98f7812b823cc4f7ad028a78403
mkarkaus <mkarkaus@Miikan-kone.localdomain>
2019-11-30 13:46:49 +0200

    clean

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mkarkaus <mkarkaus@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 16:54:39 by mkarkaus          #+#    #+#             */
-/*   Updated: 2019/11/29 11:27:03 by mkarkaus         ###   ########.fr       */
+/*   Updated: 2019/11/25 14:47:26 by mkarkaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,50 @@
 #include "libft/libft.h"
 #include <stdio.h>
 
-int		main(int argc, char **argv)
+int		main()
 {
-	char	*line;
 	int	fd;
+	int	fd2;
+	int	i;
+	char	*line;
+	char	*temp;
 
-	// int	fd2;
-	// int	i;
-	// char	*temp;
+	temp = "nuppi";
+	i = 0;
+	fd = open("text1", O_RDONLY);
+//	dup2(fd, 500);
+	fd2 = open("text4", O_RDONLY);
+	line = "nuppi";
 
-	if (argc < 2)
-		return (0);
-	// fd2 = open(argv[1], O_RDONLY);
-	// temp = " ";
-	// i = 0;
+//	get_next_line(fd, &line);
+//	printf("%s", line);
+//	free(line);
 
-	fd = open(argv[1], O_RDONLY);
-	line = " ";
-
-	// while (temp)
-	// {
-	// 	if (i % 2 == 0)
-	// 	{
-	// 		get_next_line(fd, &line);
-	// 		printf("%s", line);
-	// 	}
-	// 	if (i % 2 != 0)
-	// 	{
-	// 		get_next_line(fd2, &line);
-	// 		printf("%s", line);
-	// 	}
-	// 	temp = line;	
-	// 	if (line[0] == '\0')
-	// 		break ;
-	// 	free(line);
-	// 	printf("%c", '\n');
-	// 	i++;
-	// }
-
-	while (get_next_line(fd, &line))
+	while (temp)
 	{
-		printf("%s", line);
+		if (i % 2 == 0)
+		{
+			get_next_line(fd, &line);
+			printf("%s", line);
+		}
+		if (i % 2 != 0)
+		{
+			get_next_line(fd2, &line);
+			printf("%s", line);
+		}
+		temp = line;
+		if (line[0] == '\0')
+			break ;
+		free(line);
 		printf("%c", '\n');
-		// free(line);
+		i++;
 	}
-
-	// printf("%d\n", get_next_line(42, &line));
-
-//	while (1);
+//	get_next_line(fd, &line);
+//	printf("%s", line);
+//	free(line);
+//	while (1)
+//	{
+	
+//	}
 	return (0);
 }
